bd359047ab5f114497b1b84e5ea5fd745c069972
Mirtles <mirte.post@yahoo.co.uk>
2019-09-06 11:30:23 +0200

    Add image router.

---
+++ image/router.js
@@ -0,0 +1,15 @@
+const Router = require('express')
+const Image = require('./model')
+
+const router = new Router()
+
+router.get('/image', (req, res, next) => {
+  Image.findAll()
+    .then(images => {
+      console.table(images)
+      res.send(images)
+    })
+    .catch(err => next(err))
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const db = require('./db')
-const Image = require('./image/model')
+const imageRouter = require('./image/router')
 
 const app = express()
 const port = process.env.PORT || 4000
+
+app.use(imageRouter)
 
 app.listen(port, () => { console.table({ "Done it": `server is up and listening on port ${port}` }) })
